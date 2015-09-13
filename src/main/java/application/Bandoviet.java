7fb2a98aa4f3d704159dc88c415685f91cb89dfe
telecomparistech <ing.dev.java@gmail.com>
2015-09-14 01:50:11 +0200

    international messages

---
+++ src/main/java/application/Bandoviet.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package application;
+
+import java.util.Locale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+/**
+ * @author quocanh
+ *
+ */
+@SpringBootApplication // equal to @Configuration @EnableAutoConfiguration @ComponentScan
+public class Bandoviet extends WebMvcConfigurerAdapter {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Bandoviet.class, args);
+	}
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.US);
+        return slr;
+    }
+ 
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+ 
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/welcome.jsp
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+
+<html lang="en">
+<head>
+	<script type="text/javascript" src=/js/app.js></script>
+</head>
+<body>
+	<c:url value="/resources/text.txt" var="url"/>
+	<spring:url value="/resources/text.txt" htmlEscape="true" var="springUrl" />
+	Spring URL: ${springUrl} at ${time}
+	<br>
+	JSTL URL: ${url}
+	<br>
+	Message: ${message}
+	
+	
+	
+	<h3><spring:message code="application.title"/></h3>
+</body>
+
+</html>
